1ad3f547069626046c4181268df1cbd1e8cc9308
Alla Teterukova <51877203+aveprogramming@users.noreply.github.com>
2022-10-31 22:05:43 +0300

    Update streamlit_app.py
    moved the fruityvice code into a try-except (with a nested If-Else)

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,18 +19,20 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+# a new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# normalize the json output
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# represents the normalized output as a dataframe
-streamlit.dataframe(fruityvice_normalized)
+try: 
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice: 
+    streamlit.error("Please select a fruit to get information.")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # normalize the json output
+    streamlit.dataframe(fruityvice_normalized) # represents the normalized output as a dataframe
+ except URLError as e : 
+  streamlit.error()
+    
+#streamlit.write('The user entered ', fruit_choice)
 
  
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
